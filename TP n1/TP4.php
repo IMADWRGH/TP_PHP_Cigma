dda5ce5def2cb6493c0cc0b3579ff815ae68b013
Imad Asserne <imad.asserne@gmail.com>
2022-12-01 19:53:13 +0100

    change btn submit

--- TP n1/TP4.php
+++ TP n1/TP4.php
@@ -22,7 +22,7 @@
         <input type="number" name="nb1" value=<?= !empty($_POST) ? $_POST["nb2"] : "" ?>><br><br>
         <label for="nb2"> Nombre 2 :</label>
         <input type="number" name="nb2" value=<?= !empty($_POST) ? $_POST["nb1"] : "" ?>><br><br>
-        <input type="submit" name="sub" value=<?= !empty($_POST) && $_POST["sub"] = "Permuter" ? $_POST["sub"] = "Annuler" : $_POST["sub"] = "Premuter" ?>>
+        <input type="submit" name="sub" value="<?= !empty($_POST) ? ($_POST["sub"] == "Premuter" ? "Annuler" : "Premuter") : "Premuter" ?>">
     </form>
 </body>
 
