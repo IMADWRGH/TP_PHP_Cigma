ee85ebc2074f442ea68759a49fc8a4d9c1fdd4f7
Imad Asserne <imad.asserne@gmail.com>
2022-12-02 00:33:11 +0100

    finich_math

--- Exp_PHP/Multiply_Table.php
+++ Exp_PHP/Multiply_Table.php
@@ -9,12 +9,29 @@
 </head>
 
 <body>
-    <form action="" method="get">
-        <label for="nb1">Nombre 1:</label>
-        <input type="number" name="nb1">&scap;
-        <label for="nb1">Nombre 1:</label>
-        <input type="number" name="nb1">
+    <form method="get">
+        <label for="nb1">Nombre lignes:</label>
+        <input type="number" name="nb1" require>&nbsp;
+        <label for="nb2">Nombre colonnes:</label>
+        <input type="number" name="nb2" require>&nbsp;&nbsp;
+        <input type="submit" value="Afficher" name="btn">
     </form>
+    <table></table>
+    <?php
+    if (isset($_GET['btn'])) {
+        $color = "";
+        $nb1 = $_GET['nb1'];
+        $nb2 = $_GET['nb2'];
+        echo "<table style='width:100% ; background-color:$color;'>";
+        for ($i = 1; $i <= $nb1; $i++) {
+            echo "<tr>";
+            for ($j = 1; $j <= $nb2; $j++)
+                echo "<td>" . $i * $j . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+    ?>
 </body>
 
 </html>
