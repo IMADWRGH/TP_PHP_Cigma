278e868cc4bba4baedf71d9ad9b7b4352059cad3
Imad Asserne <imad.asserne@gmail.com>
2022-12-01 21:30:07 +0100

    first commit

---
+++ Exp_PHP/Multiply_Table.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>multiply table</title>
+</head>
+
+<body>
+    <form action="" method="get">
+        <label for="nb1">Nombre 1:</label>
+        <input type="number" name="nb1">&scap;
+        <label for="nb1">Nombre 1:</label>
+        <input type="number" name="nb1">
+    </form>
+</body>
+
+</html>
