5096f94c35fa2b8a2fed7745193c13863764dc7f
Imad Asserne <imad.asserne@gmail.com>
2022-12-03 17:29:25 +0100

    delete cmt

--- TP n2/ajout.php
+++ TP n2/ajout.php
@@ -6,6 +6,5 @@ if (isset($_POST["valider"])) {
     $con = mysqli_connect("127.0.0.1", "root", "", "tpvisiteurs");
     $req = "INSERT INTO `visiteurs`(`NOM`, `PRENOM`, `EMAIL`) VALUES ('$nom','$prenom','$email')";
     $result = mysqli_query($con, $req);
-    //var_dump($result);
     header('location : Affichage.php');
 }
