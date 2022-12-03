383a5d4328fab6f32e995957b884a49fb40d51b4
Imad Asserne <imad.asserne@gmail.com>
2022-12-03 16:01:38 +0100

    first commit

---
+++ TP n2/Affichage.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Affichage des données</title>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Num</th>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>Email</th>
+            <th>option</th>
+            <th>option</th>
+        </tr>
+        <?php
+        $con = mysqli_connect("127.0.0.1", "root", "", "tpvisiteurs");
+        $req  = "SELECT * FROM `visiteurs`";
+        $result = mysqli_query($con, $req);
+        $query_run = mysqli_query($con, $req);
+        if (mysqli_num_rows($query_run) > 0) {
+            foreach ($query_run as $data) {
+        ?>
+                <tr>
+                    <td><?= $data["ID_VISITEUR"] ?></td>
+                    <td><?= $data["NOM"] ?></td>
+                    <td><?= $data["PRENOM"] ?></td>
+                    <td><?= $data["EMAIL"] ?></td>
+                    <td><a href="Editer.php">Edit</a></td>
+                    <td><a href="Supprime.php">Sup</a></td>
+                </tr>
+        <?php
+            }
+        }
+        ?>
+    </table>
+</body>
+
+</html>
