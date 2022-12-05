0e191c961824d7e68100bf294b90fbde313b3b92
Imad Asserne <imad.asserne@gmail.com>
2022-12-05 17:24:23 +0100

    add table

---
+++ TP n2 version2/Affichage.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Affichage des données</title>
+    <style>
+        table {
+            margin: 1em;
+            border: 3px black;
+            border-style: solid;
+            max-width: 100%;
+            width: 100%;
+
+        }
+
+        th {
+            font-size: 24px;
+            background-color: gray;
+        }
+    </style>
+</head>
+
+<body>
+    <table>
+        <tr>
+            <th>Num</th>
+            <th>Nom</th>
+            <th>Prenom</th>
+            <th>E-mail</th>
+        </tr>
+        <tr>
+            <td>Num</td>
+            <td>Nom</td>
+            <td>Prenom</td>
+            <td>E-mail</td>
+        </tr>
+    </table>
+</body>
+
+</html>
