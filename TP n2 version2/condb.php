e00c63a2100fa3bf937c9a5c3b51c0d8e383019b
Imad Asserne <imad.asserne@gmail.com>
2022-12-05 15:12:16 +0100

    page condb

---
+++ TP n2 version2/condb.php
@@ -0,0 +1,9 @@
+<?php
+function ($name_db) {
+    $con = mysqli_connect("localhost", "root", "", $name_db);
+
+    if (!$con) {
+        echo "Connection Failed" . mysqli_connect_error();
+        exit();
+    }
+};
