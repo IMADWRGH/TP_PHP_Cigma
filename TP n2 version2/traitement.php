09472de578527d6471a2a0c6a5f0f10a946622c5
Imad Asserne <imad.asserne@gmail.com>
2022-12-13 18:13:26 +0100

    add f_lister

--- TP n2 version2/traitement.php
+++ TP n2 version2/traitement.php
@@ -1,18 +1,21 @@
 <?php
 include_once "condb.php";
 $con = connet("tpvisiteur");
+
 function ajoute($name, $prenom, $email)
 {
     global $con;
     $req = "INSERT INTO 'visiteurs' VALUES('$name','$prenom','$email') ";
     return mysqli_query($con, $req);
 }
+
 function modifie($num, $name, $prenom, $email)
 {
     global $con;
     $req = "UPDATE `visiteurs` SET `NOM`=$name,`PRENOM`=$prenom,`EMAIL`=$email WHERE `ID_VISITEUR`=$num ";
     return mysqli_query($con, $req);
 }
+
 function supp($num)
 {
     global $con;
@@ -24,5 +27,12 @@ function afficher($num)
 {
     global $con;
     $req = "SELECT * FROM `visiteurs` WHERE `ID_VISITEUR`= $num";
+    return mysqli_query($con, $req);
+}
+
+function lister()
+{
+    global $con;
+    $req = "SELECT * FROM `visiteurs`";
     return mysqli_query($con, $req);
 }
